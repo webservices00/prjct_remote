e1b71300645ff15f0a5816b892671db425523ed8
VilmaoWebServices <webservices00@hotmail.com>
2024-10-25 10:42:31 +0200

    LAUNCH 1.1.1 RELEASE

--- prjct_remote.py
+++ prjct_remote.py
@@ -1,6 +1,7 @@
 # Built by VilmaoTech.
 import pyautogui as pag
 import tkinter as tk
+
 # Creating the opening codes
 def max():
     print(f'OPENING: max')
@@ -25,9 +26,14 @@ def skysho():
     pag.press("win")
     pag.write("https://skyshowtime.com/watch/home")
     pag.press("enter")
+
+
 w = tk.Tk()
-w.title('PRJCT_REMOTE {0.1.1}')
+
+w.title('PC Remote')
+
 w.geometry("500x700")
+
 header = tk.Label(w, text="PRJCT_REMOTE  - HOME")
 header.pack()
 
@@ -37,11 +43,13 @@ maxbtn.pack()
 dpbtn = tk.Button(w, text="Disney+", command=dineyplus, font=("Times New Roman", 25, "bold"))
 dpbtn.pack()
 
+skyshobtn = tk.Button(w, text="SkyShotime", command=skysho, font=("Times New Roman", 25, "bold"))
+skyshobtn.pack()
+
 netflixbtn = tk.Button(w, text="Netflix", command=netflix, font=("Times New Roman", 25, "bold"))
 netflixbtn.pack()
 
-skyshobtn = tk.Button(w, text="SkyShotime", command=skysho, font=("Times New Roman", 25, "bold"))
-skyshobtn.pack()
+
 
 
 
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 **PRJCT_REMOTE is a python project to make using a computer more like using a television!**
 
-# You can build it by source or download the latest release {0.1.1}
+# You can build it by source or download the latest release {1.1.1}
 
 *Till 2025/02/20 only EU version is released!*
 
